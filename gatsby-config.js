80b01c608fdd51e1755210256f39cbc4f9dd83c7
Jason Specland <jaspecla@microsoft.com>
2019-04-28 00:03:51 -0400

    set favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,8 +65,14 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+      }
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/Jason_Specland_160120_788_LG_square_avatar.jpg"
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

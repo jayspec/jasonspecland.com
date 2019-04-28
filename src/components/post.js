81135a8c249aef9cfc6a9765d9665b86193c74b5
Jason Specland <jaspecla@microsoft.com>
2019-04-27 23:48:56 -0400

    menuItems no longer show post date metadata

--- src/components/post.js
+++ src/components/post.js
@@ -2,14 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default function Post ({post}) {
+    let dateDisplay = (
+        <div className="post-meta">
+            <span className="post-date">{post.frontmatter.date}</span>
+        </div>
+    )
+    if (post.frontmatter.layout === "page") {
+        dateDisplay = (<div></div>)
+    }
     return (
     <div className="posts">
         <div id={`post-${post.id}`} className={`post-${post.id} post type-post status-publish format-standard hentry`}>
             <div className="post-header">
                 <h1 className="post-title"><Link to={post.frontmatter.permalink}><span dangerouslySetInnerHTML={{ __html: post.frontmatter.title }}></span></Link></h1>
-                <div className="post-meta">
-                    <span className="post-date">{post.frontmatter.date}</span>
-                </div>
+                {dateDisplay}
             </div>
             <div className="post-content" dangerouslySetInnerHTML={{ __html: post.html }}></div>
             <div className="clear"></div>
